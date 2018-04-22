101637b339316546749a305f774dfa8411e096b9
hmrock <hmrock@github.com>
2018-04-22 21:52:22 +0200

    hough test with trapezoid

--- line_detection/hough_test.cpp
+++ line_detection/hough_test.cpp
@@ -1,10 +1,106 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <opencv2/imgproc/imgproc.hpp>
 
 #include "../filter/filters.cpp"
 
 using namespace cv;
 using namespace std;
+
+
+Mat crop_img(Mat frame){
+    // MOD THE IMAGE
+    cv::Mat image = frame.clone();							// C3
+    cv::Mat mask = cv::Mat::zeros(image.rows, image.cols, CV_8U); // all 0
+
+    std::string windowName = "Trapezoid";
+
+    std::vector<cv::Point> points;
+    points.push_back(cv::Point(520 , 470));   //point1
+    points.push_back(cv::Point(800 , 470));   //point2
+    points.push_back(cv::Point(1120, 665));   //point3
+    points.push_back(cv::Point(200 , 665));   //point4
+
+    cv::fillConvexPoly(mask,              // Image to be drawn on
+                       points,            // C-Style array of points
+                       //cv::Scalar(1,1,1), // Color , BGR form
+                       1,
+                       CV_AA,             // connectedness, 4 or 8
+                       0);                // Bits of radius to treat as fraction
+
+
+    image = image.mul(mask);
+    imshow("HoughLines", image);
+	while(waitKey() != 32 )
+		{continue;}
+
+    return image;
+}
+
+void test_houghLines(cv::Mat img){
+
+	cv::Mat frame = img.clone();
+	cv::Mat cframe; // cropped image
+	cv::Mat cdst, src;
+	vector<Vec4i> lines;
+
+	double rho = 10;
+	double theta = 0.5;
+	int threshold = 50; 
+
+
+	//frame = sobel_func(frame);
+	GaussianBlur( frame, frame, Size(7,7), 5, 5, BORDER_DEFAULT );
+	
+	cv::Scalar min_color = cv::Scalar(0,200,200);
+	cv::Scalar max_color = cv::Scalar(255,255,255);
+	cv::inRange(frame, min_color,  max_color, frame);
+
+	//src = frame.clone();
+	imshow("HoughLines", frame);
+	while(waitKey() != 32 )
+		{continue;}
+
+
+	//Canny(src, frame, 80,100); 
+
+	cframe = crop_img(frame);
+
+	HoughLinesP(cframe, lines, 1, CV_PI/180, 25, 30, 250 );
+	
+	cvtColor(frame, cdst, CV_GRAY2BGR);
+
+
+	//HoughLines(frame, frame, rho, theta, threshold);
+
+
+	for( size_t i = 0; i < lines.size(); i++ )
+	{
+		Vec4i l = lines[i];
+		line( cdst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, CV_AA);
+	}
+
+	imshow("HoughLines", frame);
+	while(waitKey() != 32 )
+		{continue;}
+	imshow("detected lines", cdst);
+	while(waitKey() != 32 )
+		{continue;}
+
+}
+int main(int argc, char** argv){
+
+	cv::Mat frame;
+
+	frame = imread( argv[1], IMREAD_COLOR ); // Read image
+
+	test_houghLines(frame); 
+	//imshow("detected lines", frame);
+	//while(waitKey() != 32 )
+	//	{continue;}
+
+}
+
 
 /*
 void cv::HoughLines 	( 	InputArray  	image,
@@ -18,68 +114,3 @@ void cv::HoughLines 	( 	InputArray  	image,
 		double  	max_theta = CV_PI 
 	) 		
 */
-
-void test_houghLines(cv::Mat img){
-
-	cv::Mat frame = img.clone();
-	cv::Mat cdst, src;
-	vector<Vec4i> lines;
-
-	double rho = 10;
-	double theta = 0.5;
-	int threshold = 50; 
-
-	//frame = sobel_func(frame);
-	//cv::Scalar min_color = cv::Scalar(0,200,200);
-	//cv::Scalar max_color = cv::Scalar(255,255,255);
-	//cv::inRange(frame, min_color,  max_color, frame);
-
-	src = frame.clone();
-
-	GaussianBlur( src, src, Size(7,7), 2, 2, BORDER_DEFAULT );
-	GaussianBlur( src, src, Size(7,7), 2, 2, BORDER_DEFAULT );
-	GaussianBlur( src, src, Size(7,7), 2, 2, BORDER_DEFAULT );
-	GaussianBlur( src, src, Size(7,7), 2, 2, BORDER_DEFAULT );
-	GaussianBlur( src, src, Size(7,7), 2, 2, BORDER_DEFAULT );
-	GaussianBlur( src, src, Size(7,7), 2, 2, BORDER_DEFAULT );
-	GaussianBlur( src, src, Size(7,7), 2, 2, BORDER_DEFAULT );
-	GaussianBlur( src, src, Size(7,7), 2, 2, BORDER_DEFAULT );
-	GaussianBlur( src, src, Size(7,7), 2, 2, BORDER_DEFAULT );
-	GaussianBlur( src, src, Size(7,7), 2, 2, BORDER_DEFAULT );
-
-	//Canny(src, frame, 80,100); 
-
-	
-
-
-	//HoughLinesP(frame, lines, 1, CV_PI/180, 10, 50, 50 );
-	//HoughLines(frame, frame, rho, theta, threshold);
-	//cvtColor(frame, cdst, CV_GRAY2BGR);
-
-
-	for( size_t i = 0; i < lines.size(); i++ )
-	{
-		Vec4i l = lines[i];
-		line( cdst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, CV_AA);
-	}
-
-	imshow("HoughLines", src);
-	while(waitKey() != 32 )
-		{continue;}
-	imshow("detected lines", cdst);
-
-
-	while(waitKey() != 32 )
-		{continue;}
-
-}
-
-int main(int argc, char** argv){
-
-	cv::Mat frame;
-
-	frame = imread( argv[1], IMREAD_COLOR ); // Read image
-
-	test_houghLines(frame); 
-
-}
